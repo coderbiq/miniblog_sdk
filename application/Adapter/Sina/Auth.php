2d932734e83c9c32726218f7577c94a0ffe45a76
elvis-bi <elvis@dwenzi.com>
2011-04-12 22:25:59 +0800

    实现公共授权接口

--- application/Adapter/Sina/Auth.php
+++ application/Adapter/Sina/Auth.php
@@ -2,6 +2,12 @@
 # 定义命名空间
 namespace Ebsdk\Adapter\Sina;
 
-class Auth
+# 包含依赖文件
+require_once __DIR__ . '/../../Auth.php';
+
+class Auth implements \Ebsdk\Auth
 {
+    public function __construct(Array $_config)
+    {
+    }
 }
--- application/Adapter/Tencent/Auth.php
+++ application/Adapter/Tencent/Auth.php
@@ -2,6 +2,12 @@
 # 定义命名空间
 namespace Ebsdk\Adapter\Tencent;
 
-class Auth
+# 包含依赖文件
+require_once __DIR__ . '/../../Auth.php';
+
+class Auth implements \Ebsdk\Auth
 {
+    public function __construct(Array $_config)
+    {
+    }
 }
