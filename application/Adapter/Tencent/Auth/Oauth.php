070d82e4776c1b99f84a0a8f5e831689b91c7970
elvis-bi <elvis@dwenzi.com>
2011-04-12 23:43:59 +0800

    将Tencent\Auth更名为Tencent\Auth\Oauth并实现Ebsdk\Auth\Oauth

---
+++ application/Adapter/Tencent/Auth/Oauth.php
@@ -0,0 +1,34 @@
+<?php
+# 定义命名空间
+namespace Ebsdk\Adapter\Tencent\Auth;
+
+# 包含依赖文件
+require_once __DIR__ . '/../../../Auth.php';
+require_once __DIR__ . '/../../../Auth/Oauth.php';
+
+class Oauth implements \Ebsdk\Auth, \Ebsdk\Auth\Oauth
+{
+    public function getRequestToken($_callback)
+    {
+    }
+
+    public function getAuthorizeURL($_token, $_sign_in_with_Weibo = TRUE , $_url)
+    {
+    }
+
+    public function getAccessToken($_oauth_verifier = FALSE, $_oauth_token = false)
+    {
+    }
+
+    public function setToken($_token)
+    {
+    }
+
+    public function setTokenSecret($_token_secret)
+    {
+    }
+
+    public function __construct(Array $_config)
+    {
+    }
+}
--- tests/Adapter/Tencent/Client.php
+++ tests/Adapter/Tencent/Client.php
@@ -30,6 +30,6 @@ class Client extends \Test\Client\Base
 
     protected function _assertAuthObject($_auth)
     {
-        $this->assertType('\Ebsdk\Adapter\Tencent\Auth', $_auth);
+        $this->assertType('\Ebsdk\Adapter\Tencent\Auth\Oauth', $_auth);
     }
 }
