3554ee5abf6fb398b2607e537071c87fe2a5df69
elvis-bi <elvis@dwenzi.com>
2011-04-12 22:42:00 +0800

    定义oauth授权接口的公共行为

---
+++ application/Auth/Oauth.php
@@ -0,0 +1,21 @@
+<?php
+# 定义命名空间
+namespace Ebsdk\Auth
+
+/**
+ * 接口 Oauth 定义oauth授权接口的公共行为
+ * 
+ * @package Ebsdk\Auth
+ * @version 0.1
+ * @copyright 2010 www.dwenzi.com
+ * @author elvis <elvis@dwenzi.com> 
+ * @license New BSD License  {@link http://creativecommons.org/licenses/by-nc-sa/2.5/cn/}
+ */
+interface Oauth
+{
+    public function getRequestToken($_callback);
+    public function getAuthorizeURL($_token, $_sign_in_with_Weibo = TRUE , $_url);
+    public function getAccessToken($_oauth_verifier = FALSE, $_oauth_token = false);
+    public function setToken($_token);
+    public function setTokenSecret($_token_secret);
+}
