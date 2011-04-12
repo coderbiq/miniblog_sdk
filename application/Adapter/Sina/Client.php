1c71b3a05d2ac8808f832986c577cd865664a9cf
elvis-bi <elvis@dwenzi.com>
2011-04-12 20:59:17 +0800

    继承至基本的客户端类抽象

--- application/Adapter/Sina/Client.php
+++ application/Adapter/Sina/Client.php
@@ -3,49 +3,22 @@
 namespace Ebsdk\Adapter\Sina;
 
 # 包含依赖文件
-require_once __DIR__ . '/../../Client.php';
+require_once __DIR__ . '/../../Client/Base.php';
 require_once __DIR__ . '/Auth.php';
 
-class Client implements \Ebsdk\Client
+/**
+ * 类 Client 封装新浪微博客户端
+ * 
+ * @package \Ebsdk\Adapter\Sina
+ * @version 0.1
+ * @copyright 2010 www.dwenzi.com
+ * @author elvis <elvis@dwenzi.com> 
+ * @license New BSD License  {@link http://creativecommons.org/licenses/by-nc-sa/2.5/cn/}
+ */
+class Client extends \Ebsdk\Client\Base
 {
-    protected $_app_key;
-    protected $_app_secret;
-
-    protected $_objects = array();
-
-    public function getAuth($_type = 'oauth', $_config = array())
+    protected function _getAuthClassName()
     {
-        if(!isset($this->_objects['oauth']))
-        {
-            $base_config = array(
-                'app_key' => $this->_app_key,
-                'app_secret' => $this->_app_secret
-            );
-            $config = array_merge($base_config, $_config);
-
-            $this->_objects['oauth'] = new Auth($config);
-        }
-
-        return $this->_objects['oauth'];
-    }
-
-    public function setAppkey($_app_key)
-    {
-        $this->_app_key = $_app_key;
-
-        return $this;
-    }
-
-    public function setSecret($_app_secret)
-    {
-        $this->_app_secret = $_app_secret;
-
-        return $this;
-    }
-
-    public function __construct(Array $_config)
-    {
-        $this->setAppKey($_config['app_key'])
-            ->setSecret($_config['app_secret']);
+        return __NAMESPACE__ . '\\Auth';
     }
 }
--- application/Adapter/Tencent/Client.php
+++ application/Adapter/Tencent/Client.php
@@ -3,7 +3,7 @@
 namespace Ebsdk\Adapter\Tencent;
 
 # 包含依赖文件
-require_once __DIR__ . '/../../Client.php';
+require_once __DIR__ . '/../../Client/Base.php';
 require_once __DIR__ . '/Auth.php';
 
 /**
@@ -15,46 +15,10 @@ require_once __DIR__ . '/Auth.php';
  * @author elvis <elvis@dwenzi.com> 
  * @license New BSD License  {@link http://creativecommons.org/licenses/by-nc-sa/2.5/cn/}
  */
-class Client implements \Ebsdk\Client
+class Client extends \Ebsdk\Client\Base
 {
-    protected $_app_key;
-    protected $_app_secret;
-
-    protected $_objects = array();
-
-    public function getAuth($_type = 'oauth', $_config = array())
+    protected function _getAuthClassName()
     {
-        if(!isset($this->_objects['oauth']))
-        {
-            $base_config = array(
-                'app_key' => $this->_app_key,
-                'app_secret' => $this->_app_secret
-            );
-            $config = array_merge($base_config, $_config);
-
-            $this->_objects['oauth'] = new Auth($config);
-        }
-
-        return $this->_objects['oauth'];
-    }
-
-    public function setAppKey($_app_key)
-    {
-        $this->_app_key = $_app_key;
-
-        return $this;
-    }
-
-    public function setSecret($_app_secret)
-    {
-        $this->_app_secret = $_app_secret;
-
-        return $this;
-    }
-
-    public function __construct(Array $_config)
-    {
-        $this->setAppKey($_config['app_key'])
-            ->setSecret($_config['app_secret']);
+        return __NAMESPACE__ . '\\Auth';
     }
 }
